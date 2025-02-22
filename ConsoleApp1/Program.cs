8ec5eadc197a85ee833758ecf4df580b2f572b80
Ajaysharma3302 <sharma09443302@gmail.com>
2025-02-22 23:42:18 +0530

    added assignment of dotnet

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LibraryManagementSystem
+{
+
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; }
+
+
+        public Book(int bookID, string title, string author)
+        {
+            BookID = bookID;
+            Title = title;
+            Author = author;
+            IsAvailable = true;
+        }
+
+
+        public void DisplayBookDetails()
+        {
+            Console.WriteLine($"BookID: {BookID}, Title: {Title}, Author: {Author}");
+        }
+    }
+}
